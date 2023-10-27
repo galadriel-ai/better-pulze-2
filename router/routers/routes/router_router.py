a61aae3d918debe6ce8321a6a70250e7ea8fec18
kristjanpeterson <kristjan@nftport.xyz>
2023-10-27 15:59:17 -0700

    Streaming support

--- router/routers/routes/router_router.py
+++ router/routers/routes/router_router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Header
+from starlette.responses import StreamingResponse
 
 from router import api_logger
-from router.service.completion import completion_service
+from router.service.completion import completion_service, completion_stream_service
 from router.service.completion.entities import ChatCompletionRequest, ChatCompletionResponse
 
 TAG = "Router"
@@ -14,11 +15,21 @@ logger = api_logger.get()
 
 @router.post(
     "/v1/chat/completions",
-    response_model=ChatCompletionResponse,
+    # response_model=ChatCompletionResponse,
 )
 async def endpoint(
         request: ChatCompletionRequest,
         authorization: str = Header(None)
 ):
-    return await completion_service.execute(request)
-
+    if not request.stream:
+        return await completion_service.execute(request)
+    else:
+        headers = {
+            "X-Content-Type-Options": "nosniff",
+            "Connection": "keep-alive",
+        }
+        return StreamingResponse(
+            completion_stream_service.execute(request),
+            headers=headers,
+            media_type="text/event-stream",
+        )
---
+++ router/service/completion/completion_stream_service.py
@@ -0,0 +1,78 @@
+import json
+import os
+from typing import Dict, AsyncIterable
+
+import aiohttp
+from langsmith import traceable
+
+from router.domain.pricing.entities import UsageDebug
+from router.service.completion.entities import ChatCompletionRequest
+from router.service.completion.intent_router import detect_intent, Intent
+
+
+async def execute(request: ChatCompletionRequest, authorization=None) -> AsyncIterable:
+    # Clean up etc
+    request.model = "gpt-4"  # TODO: pick model in a smart way :)
+    request_input = request.model_dump()
+    intent, intent_usage = detect_intent(request_input["messages"][-1]["content"])
+    if intent == Intent.REASONING:
+        request.model = "gpt-4"
+    else:
+        request.model = "gpt-3.5-turbo-16k"
+    for m in request_input["messages"]:
+        if not m.get("function_call"):
+            m.pop("name", None)
+            m.pop("function_call", None)
+
+    # OpenAI API is annoying
+    formatted_dict = {}
+    for key, value in request_input.items():
+        if value:
+            formatted_dict[key] = value
+
+    all_lines = []
+    async with aiohttp.ClientSession() as session:
+        res = await session.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers={
+                "Authorization": authorization if authorization else f'Bearer {os.getenv("OPENAI_API_KEY")}'
+            },
+            json=formatted_dict
+        )
+        async for line in res.content:
+            try:
+                decoded = line.decode()
+                if decoded[1] != "\n":
+                    all_lines.append(json.loads(decoded.split("data: ")[-1]))
+            except:
+                pass
+            yield line
+
+    @traceable(run_type="llm", name="stream_openai.ChatCompletion.create")
+    def trace(r):
+        try:
+            model = all_lines[0]["model"]
+            content = "".join([l['choices'][0]["delta"].get("content", "") for l in all_lines])
+            return model, content
+        except:
+            return "", ""
+
+    trace(request)
+
+
+async def _get_oai_streaming_response(authorization, formatted_dict):
+    async with aiohttp.ClientSession() as session:
+        return session.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers={
+                "Authorization": authorization if authorization else f'Bearer {os.getenv("OPENAI_API_KEY")}'
+            },
+            json=formatted_dict
+        )
+
+
+def _get_usage_response(usage: UsageDebug, usage_type: str) -> Dict:
+    return {
+        "type": usage_type,
+        **usage.__dict__
+    }
