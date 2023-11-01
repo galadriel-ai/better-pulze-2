d6dd6389f6d1e3f03011a4b6e7d69bd6b3e4158b
Kaspar Peterson <kasparpeterson@hotmail.com>
2023-11-01 10:43:23 -0700

    Adds Stytch user auth.

--- router/service/auth/validate_id_token.py
+++ router/service/auth/validate_id_token.py
@@ -1,29 +1,53 @@
+from abc import ABC
+from abc import abstractmethod
 from typing import Optional
 
 from fastapi import Security
 from fastapi.security import APIKeyHeader
+from stytch import Client
+from stytch.consumer.models.sessions import AuthenticateResponse
 
+from router import api_logger
 from router.repository.user_repository import UserRepositoryFirebase
+from router.repository.user_repository import ValidatedUser
 from router.service import error_responses
 
 API_KEY_NAME = "Authorization"
 API_KEY_HEADER = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
+logger = api_logger.get()
 
-class IdTokenValidator:
-    def __init__(self, user_repository: UserRepositoryFirebase):
-        self.user_repository = user_repository
+
+class SessionTokenValidator(ABC):
+    @abstractmethod
+    async def validate(
+        self, api_key_header: str = Security(API_KEY_HEADER)
+    ) -> Optional[ValidatedUser]:
+        pass
+
+
+class StytchTokenValidator(SessionTokenValidator):
+    def __init__(self, project_id: str, secret: str, environment: str = "test"):
+        self.client = Client(
+            project_id=project_id,
+            secret=secret,
+            environment=environment,
+        )
 
     async def validate(
         self, api_key_header: str = Security(API_KEY_HEADER)
-    ) -> Optional[str]:
+    ) -> Optional[ValidatedUser]:
         try:
             if not api_key_header:
                 raise error_responses.AuthorizationMissingAPIError()
 
-            validated_user = self.user_repository.validate_user(api_key_header)
-            return validated_user
+            resp: AuthenticateResponse = self.client.sessions.authenticate(
+                session_token=api_key_header
+            )
+            user = resp.user
+            return ValidatedUser(uid=user.user_id, email=user.emails[0].email)
         except Exception as exc:
+            logger.debug(f"StytchTokenValidator.validate exception: {exc}")
             raise error_responses.InvalidCredentialsAPIError()
 
 
